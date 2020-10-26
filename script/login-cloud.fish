8ebe9e7e8a5db1104c5947d06825f425e9ab7c35
shinyay <shinya.com@gmail.com>
2020-10-26 16:20:11 +0900

    Add display project id set

--- script/login-cloud.fish
+++ script/login-cloud.fish
@@ -10,8 +10,6 @@ function do_func
   argparse -n do_func 'h/help' 'u/user=' 'p/project=' -- $argv
   or return 1
 
-
-
   if set -lq _flag_help
     echo "login-cloud.fish -u/--user <LOGIN_USER> -p/--project <PROJECT_ID>"
     return
@@ -25,6 +23,7 @@ function do_func
   gcloud components update --quiet
   gcloud auth login $_flag_user
   gcloud config set project $_flag_project
+  gcloud config get-value project
 end
 
 do_func $argv
