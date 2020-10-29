c5ea27cd6fe252abc474a3ce59f82c31d241cdc0
shinyay <shinya.com@gmail.com>
2020-10-29 09:11:46 +0900

    Modify download key file

--- script/configure-service-account.fish
+++ script/configure-service-account.fish
@@ -30,15 +30,16 @@ function do_func
     gcloud projects add-iam-policy-binding (gcloud config get-value project)  \
       --member=serviceAccount:m4a-install@(gcloud config get-value project).iam.gserviceaccount.com \
       --role=roles/storage.admin
-    gcloud iam service-accounts keys create m4a-install.json \
-      --iam-account=m4a-install@(gcloud config get-value project).iam.gserviceaccount.com \
-      --project=(gcloud config get-value project)
     #### To use Compute Engine as a migration source
     gcloud iam service-accounts create m4a-ce-src \
       --project=(gcloud config get-value project)
     gcloud projects add-iam-policy-binding (gcloud config get-value project)  \
       --member=serviceAccount:m4a-ce-src@(gcloud config get-value project).iam.gserviceaccount.com \
       --role=roles/compute.admin
+    #### Download Key files for the service account
+    gcloud iam service-accounts keys create m4a-install.json \
+      --iam-account=m4a-install@(gcloud config get-value project).iam.gserviceaccount.com \
+      --project=(gcloud config get-value project)
     gcloud iam service-accounts keys create m4a-ce-src.json \
       --iam-account=m4a-ce-src@(gcloud config get-value project).iam.gserviceaccount.com \
       --project=(gcloud config get-value project)
